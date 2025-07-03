17008d84cbd5d31f658054ee912ba79e7a3a6d4c
jukvalim <jukka@jukvalim.net>
2025-07-03 22:38:56 +0300

    Add Helsinki Zouk links to main page as well

---
+++ src/lib/ZoukLinks.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import { m } from '$lib/paraglide/messages.js';
+
+	const links = [
+		{
+			href: "https://calendar.google.com/calendar/u/0?cid=dWp1bGo4NzgyczBkcm1xYzlxcXI3b3E3MDhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+			title: m['links.calendar']()
+		},
+		{
+			href: "https://www.facebook.com/groups/159001944476740",
+			title: m['links.facebook_group']()
+		},
+		{
+			href: "https://www.helsinkidancecentral.com/community-guidelines",
+			title: m['links.guidelines']()
+		},
+		{
+			href: "http://facebook.com/profile.php?id=61551733333069",
+			title: m['links.zerotohero_page']()
+		},
+		{
+			href: "https://www.helsinkidancecentral.com",
+			title: m['links.hdc']()
+		},
+		{
+			href: "https://www.facebook.com/lambadahelsinki",
+			title: m['links.lambada'](),
+			extraInfo: m['links.lambada_extra_info']()
+		},
+		{
+			href: "https://www.instagram.com/helsinki_zouk/",
+			title: m['links.helsinki_zouk_instagram']()
+		}
+	];
+</script>
+
+<h2 class="text-2xl font-bold mb-2">{m['links_title']()}</h2>
+
+<ul class="space-y-2">
+	{#each links as link}
+		<li>
+			<a href={link.href}>{link.title}</a>{#if link.extraInfo}&nbsp;<span class="text-sm text-gray-500">({link.extraInfo})</span>{/if}
+		</li>
+	{/each}
+</ul>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
 
 	import { m } from '$lib/paraglide/messages.js';
+	import ZoukLinks from '$lib/ZoukLinks.svelte';
 </script>
 
 
-<h1 class="text-4xl font-bold">Zouk Zero to Hero</h1>
-<p>{m.zouk_zero_to_hero_description()}</p>
+<h1 class="text-4xl font-bold mb-4">Zouk Zero to Hero</h1>
+<p class="mb-8">{m.zouk_zero_to_hero_description()}</p>
+
+<ZoukLinks />
