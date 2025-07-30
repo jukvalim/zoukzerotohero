0772ec8323deeda521773f042910a7e5f944d8c4
jukvalim <jukka@jukvalim.net>
2025-07-30 12:05:28 +0300

    Add links to beginner courses to welcome page

--- src/routes/welcome/+page.svelte
+++ src/routes/welcome/+page.svelte
@@ -8,4 +8,39 @@
 	{m['welcome.welcome_description_1']()} <a href="https://www.facebook.com/events/1949014439171820/">{m['welcome.welcome_description_2']()}</a>.
 </p>
 
+<h2 class="md:text-2xl text-xl font-bold mb-4">
+	{m['welcome.beginner_courses_title']()
+}</h2>
+
+<p class="mb-4">
+	{m['welcome.beginner_courses_description']()}
+</p>
+
+<ul class="space-y-2 mb-8">
+	<li>
+		<a href="https://fb.me/e/6GvMQxHUa">
+			{m['welcome.beginners_zzh']()}
+		</a>
+		{m['welcome.beginners_zzh_description']()}
+	</li>
+	<li>
+		<a href="https://www.helsinkidancecentral.com/courses-events/zoukprogressivecourses">
+			{m['welcome.welcome_beginners_hdc']()}
+		</a>
+		{m['welcome.welcome_beginners_hdc_description']()}
+	</li>
+	<li>
+		<a href="https://fb.me/e/5ULvt1Ekh">
+			{m['welcome.welcome_beginners_lds']()}
+		</a>
+		{m['welcome.welcome_beginners_lda_description']()}
+	</li>
+	<li>
+		<a href="https://www.instagram.com/p/DMK2X5SI0Y5">
+			{m['welcome.welcome_beginners_lambada']()}
+		</a>
+		{m['welcome.welcome_beginners_lambada_description']()}
+	</li>
+</ul>
+
 <ZoukLinks />
