fc630a1cd9c7dabcfc38dd1bc522d67d4047d783
jukvalim <jukka@jukvalim.net>
2025-08-01 10:49:05 +0300

    Add head movements course page

---
+++ src/routes/head-movements/+page.svelte
@@ -0,0 +1,75 @@
+<script lang="ts">
+	import { m } from '$lib/paraglide/messages.js';
+</script>
+
+<h1 class="md:text-4xl text-2xl font-bold mb-8">Head Movements &dash; Brazilian Zouk Course</h1>
+
+<h2 class="md:text-2xl text-xl font-bold mb-4">Learn Brazilian Zouk Head Movements</h2>
+
+<p class="mb-2">
+	Fluid and beautiful head movements are one of the most distinctive characteristics of Brazilian Zouk. Would you like to learn them? In this course, we’ll start from the very basics of head movements, and progress with different types of head movements we use in Zouk:
+</p>
+
+<ul class="mb-2">
+	<li>Vertical head movements</li>
+	<li>Horizontal head movements (Frango Assado/Rotisserie)</li>
+	<li>Head movements with steps</li>
+	<li>Tilted turns</li>
+</ul>
+
+<p class="mb-4">
+	<b>This course is not for beginners.</b> As a prerequisite, you should be able to do the following movements: Basic step, Viradinha, Lateral, Preparation (aka Lunge), Simple Turn and Pião.
+</p>
+
+<h2 class="md:text-2xl text-xl font-bold mb-4">What You'll Get</h2>
+
+<ul class="mb-4">
+	<li>6 structured weekly classes (1.5 hours each, Mondays)</li>
+	<li>Recap videos after each class</li>
+	<li>Personalized feedback</li>
+</ul>
+
+<h2 class="md:text-2xl text-xl font-bold mb-4">Course Details</h2>
+
+<ul class="mb-4">
+	<li><b>Teachers:</b> Jukka and Anna</li>
+	<li><b>Start date:</b> Monday, September 1st</li>
+	<li><b>Duration:</b> 6 weeks</li>
+	<li><b>Time:</b> Mondays, 19:30-21:00</li>
+	<li><b>Location:</b> <a href="https://www.helsinkidancecentral.com/">Helsinki Dance Central</a>, Sörnaisten Rantatie 33 C, 4th Floor</li>
+	<li><b>Teaching language:</b> English</li>
+	<li><b>Price:</b> 130€, couple registration 240€</li>
+</ul>
+
+<h2 class="md:text-2xl text-xl font-bold mb-6">Sign up Now</h2>
+
+<a href="https://holvi.com/shop/zoukzerotohero/product/0a9af3fbd73e283bd0cbcd74d6aac55e/" class="bg-blue-500 text-white px-4 py-2 rounded-md m-4">Register as a follower</a>
+<br>
+<br>
+<a href="https://holvi.com/shop/zoukzerotohero/product/0cd1ff232e16afa16c99340f92527ba6/" class="bg-blue-500 text-white px-4 py-2 rounded-md m-4">Register as a leader</a>
+<br>
+<br>
+<a href="https://holvi.com/shop/zoukzerotohero/product/02ea8d3cc397c67d7b1c24e1ea176137" class="bg-blue-500 text-white px-4 py-2 rounded-md m-4">Couple registration</a>
+
+
+<hr class="my-8">
+
+<h2 class="md:text-2xl text-xl font-bold mb-4">🤝 Spread the Zouk &dash; Referral Program & Discounts</h2>
+
+<p>Would you like to help us grow the Zouk community? Invite your friends to our <a href="/beginners">Zero to Zouk beginner course</a> and you'll get a 20€ discount for the head movements course.</p>
+
+<ul>
+	<li>Ask us for your personal <b>discount code</b> to share</li>
+	<li>For each person who signs up using your code, you get:
+		<ul>
+			<li>A <b>20€ discount</b> on head movements course</li>
+			<li>Alternatively, a <b>15€ discount</b> on a private class with Jukka or Anna (1h)</li>
+		</ul>
+	</li>
+	<li>The person who signed up for Zero to Zouk beginner course gets:
+		<ul>
+			<li>A <b>30€ discount</b> on the course price</li>
+			<li>They get to learn Brazilian Zouk!</li>
+		</ul>
+	</li>
+</ul>
